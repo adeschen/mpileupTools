2f72caeefff3d9e281d4f311636982ce872ee238
adeschen <adeschen@hotmail.com>
2017-09-19 14:32:53 -0400

    Adding unit test test_extractCigarSeq_ref_and_one_alternative_allele()

--- unit/parseMPileup_test.py
+++ unit/parseMPileup_test.py
@@ -19,6 +19,21 @@ class ExtractCigarSeqTestCase(unittest.TestCase):
         self.assertTrue(lettersCount['T'] == [])
         self.assertTrue(lettersCount['N'] == [])
         self.assertTrue(lettersCount['O'] == [])
+    
+    def test_extractCigarSeq_ref_and_one_alternative_allele(self):
+        """Test a cigar sequence that has the reference allele on forward and reverse strand and one alternative allele"""
+        sequence = ".,T.,"
+        phred = "DF!DG"
+        mapq = "]]acb"
+        info = dict([('chr', "2"), ('pos', "323"), ('ref', "C"), ('NB', "4")])
+        lettersCount = extractCigarSeq(sequence, phred, mapq, info)
+        self.assertTrue(len(lettersCount) == 6)
+        self.assertTrue(lettersCount['A'] == [])
+        self.assertTrue(lettersCount['C'] == [(35, 60), (37, 60), (35, 66), (38, 65)])
+        self.assertTrue(lettersCount['G'] == [])
+        self.assertTrue(lettersCount['T'] == [(0, 64)])
+        self.assertTrue(lettersCount['N'] == [])
+        self.assertTrue(lettersCount['O'] == [])
         
 if __name__ == '__main__':
     unittest.main()
